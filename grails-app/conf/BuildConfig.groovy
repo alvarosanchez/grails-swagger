e3ca53c9e7d7670b094b4d6786be48426912b5e1
mario <mario@pigheaded.pigheaded.kaleidos.net>
2013-12-12 19:37:55 +0100

    Added service that lists url mappings

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,11 +31,18 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         compile 'com.wordnik:swagger-core_2.10:1.3.1'
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
+        test(":spock:0.7") {
+            exclude("spock-grails-support")
+        }
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
+            export = false
+        }
+        runtime(":hibernate:3.6.10.6") {
             export = false
         }
         runtime ":resources:1.2.1"
---
+++ grails-app/services/org/grails/plugins/swagger/ResourcesListService.groovy
@@ -0,0 +1,35 @@
+package org.grails.plugins.swagger
+
+import grails.converters.JSON
+
+class ResourcesListService {
+
+    def grailsApplication
+    def grailsUrlMappingsHolder
+
+	def listAll() {
+
+        /* This gets all urlMappings */
+        def urlMappingsCollector = { [
+          path: it.urlData.logicalUrls.first()
+        ] }
+
+        def urlMappings =
+            grailsUrlMappingsHolder.urlMappings.collect(urlMappingsCollector)
+
+        /* Gathering all information */
+        def root =
+            grailsApplication.with {
+                return [
+                    apiVersion: metadata['app.version'],
+                    swaggerVersion : config.swaggerVersion ,
+                    apis: urlMappings
+                ]
+            }
+
+        return root
+
+	}
+
+
+}
